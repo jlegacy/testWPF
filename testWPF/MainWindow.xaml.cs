7f26eefbd8136c51bef15626a8a6a3f7d5dffc17
jlegacy <jlegacy@suddenlink.net>
2014-05-28 13:36:47 -0400

    initial commit

---
+++ testWPF/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace testWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+
+       
+    {
+        public string FirstName { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (Validator.IsValid(this)) // is valid
+   {
+
+   
+   }
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ testWPF/NameValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Windows.Controls;
+
+namespace testWPF
+{
+    public class NameValidator : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value.ToString().Trim().Length == 0)
+                return new ValidationResult(false, "value cannot be empty.");
+            return ValidationResult.ValidResult;
+        }
+    }
+}
